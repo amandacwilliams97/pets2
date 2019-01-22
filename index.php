7a7d01c2553bd70adaf493659ef482b2085d195c
Amanda Williams <amandacwilliams97@gmail.com>
2019-01-22 17:05:31 -0500

    results page displays thank you message

--- index.php
+++ index.php
@@ -12,12 +12,14 @@ $f3 = Base::instance();
 #Turn on Fat-Free error reporting
 $f3->set('DEBUG', 3);
 
+#----------------------------------------------------------------
 #define a default route
 $f3->route('GET /', function() {
     echo '<h1>My Pets</h1>';
     echo "<a href='order'>Order a Pet</a>";
 });
 
+#----------------------------------------------------------------
 #define a route that accepts a parameter for animal type
 $f3->route('GET /@animal', function($f3, $params) {
     $animals = ['dog', 'cat', 'duck', 'cow', 'chicken'];
@@ -44,12 +46,12 @@ $f3->route('GET /@animal', function($f3, $params) {
     }
 });
 
-#---------------------------------------------------------------
+#----------------------------------------------------------------
 #define a route /order that renders form1.html
 $f3->route('GET /order', function() {
     $view = new View();
     echo $view->render('views/form1.html');
-});
+});#to form1
 
 #define a route /order2 that uses POST
 $f3->route('POST /order2', function() {
@@ -59,7 +61,16 @@ $f3->route('POST /order2', function() {
 
     $view = new View();
     echo $view->render('views/form2.html');
-});
+});#from form1 to form2
+
+#define route for results
+$f3->route('POST /results', function() {
+    $_SESSION['color'] = $_POST['color'];
+
+    print_r($_SESSION);//why did animal disappear?
+    $template=new Template();
+    echo $template->render('views/results.html');
+});#from form 2 to results
 
 #run fat free
 $f3->run();
