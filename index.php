a34389782915e7df84645f2c4f21279b5dc694cc
Amanda Williams <amandacwilliams97@gmail.com>
2019-02-01 13:35:45 -0500

    Added template to form2

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $f3->set('DEBUG', 3);
 #define a default route
 $f3->route('GET /', function() {
     echo '<h1>My Pets</h1>';
-    echo "<a href='order'>Order a Pet</a>";
+    echo "<a href='order'>Order a pet</a>";
 });
 
 #----------------------------------------------------------------
@@ -56,14 +56,14 @@ $f3->route('GET /order', function($f3) {
     $view = new View();
     echo $view->render('views/form1.html');
 
-    $f3->set('colors', array('pink', 'green', 'blue'));
-
 });#to form1
 
 #define a route /order2 that uses POST
-$f3->route('POST /order2', function() {
+$f3->route('POST /order2', function($f3) {
     $_SESSION['animal'] = $_POST['animal'];
-    $view = new View();
+
+    $f3->set('colors', array('pink', 'green', 'blue'));
+    $view = new Template();
     echo $view->render('views/form2.html');
 });#from form1 to form2
 
